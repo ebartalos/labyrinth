d50dd9f163f468e091ae5f99d68ddfda36d85911
Erik Bartalos <erik@thinkcreatix.com>
2017-08-14 15:28:49 +0200

    Add fastest way finder

--- labyrinth.py
+++ labyrinth.py
@@ -1,4 +1,12 @@
+import copy
+
+
 def isEmpty(s):
+    """
+    Finds out if the point is empty.
+    :param s: string.
+    :return: bool.
+    """
     if isinstance(s, str):
         return not bool(s and s.strip())
     else:
@@ -6,13 +14,26 @@ def isEmpty(s):
 
 
 def isDistanceLower(_matrix_index, _distance):
+    """
+    Compare distance in the current flooding iteration and in the matrix map.
+    :param _matrix_index: matrix map distance. Constant.
+    :param _distance: current flooding iteration. Constant.
+    :return: true if the distance is lower in current flooding iteration.
+    """
     if isinstance(_matrix_index, str) and _matrix_index == '*':
         return False
     if isinstance(_matrix_index, int):
         return _matrix_index > _distance
 
 
-def findPath(_start_index_x, _start_index_y, _distance):
+def flood(_start_index_x, _start_index_y, _distance):
+    """
+    Flooding algorithm to fill every gap in the labyring with the distance.
+    :param _start_index_x: X index of matrix. Variable.
+    :param _start_index_y: Y index of matrix. Variable.
+    :param _distance: Distance from the beginning. Variable.
+    :return: Nothing.
+    """
     global matrix
     matrix_pointers = [(_start_index_x + 1, _start_index_y), (_start_index_x - 1, _start_index_y),
                        (_start_index_x, _start_index_y + 1), (_start_index_x, _start_index_y - 1)]
@@ -21,8 +42,44 @@ def findPath(_start_index_x, _start_index_y, _distance):
         if isEmpty(matrix[matrix_pointer[0]][matrix_pointer[1]]) or isDistanceLower(
                 matrix[matrix_pointer[0]][matrix_pointer[1]], _distance):
             matrix[matrix_pointer[0]][matrix_pointer[1]] = _distance
-            findPath(matrix_pointer[0], matrix_pointer[1], _distance + 1)
+            flood(matrix_pointer[0], matrix_pointer[1], _distance + 1)
 
+
+def find_all_ways(matrix, _start_index_x, _start_index_y, _distance, _way_previous):
+    """
+    Finds all ways from the A to the B point.
+    :param matrix: resolved matrix. Constant.
+    :param _start_index_x: X index of matrix. Variable.
+    :param _start_index_y: Y index of matrix. Variable.
+    :param _distance: Distance from the beginning. Variable.
+    :param _way_previous: Previous path to actual point. Variable.
+    :return: Just to break when the final path is found.
+    """
+    global all_ways
+
+    matrix_pointers = [(_start_index_x + 1, _start_index_y), (_start_index_x - 1, _start_index_y),
+                       (_start_index_x, _start_index_y + 1), (_start_index_x, _start_index_y - 1)]
+
+    for matrix_pointer in matrix_pointers:
+        if matrix[matrix_pointer[0]][matrix_pointer[1]] == "b":
+            _way_previous.append(matrix_pointer)
+            all_ways.append(_way_previous)
+            return
+        if matrix[matrix_pointer[0]][matrix_pointer[1]] == _distance + 1:
+            _way = copy.copy(_way_previous)
+            _way.append(matrix_pointer)
+            find_all_ways(matrix, matrix_pointer[0], matrix_pointer[1], _distance + 1, _way)
+
+
+def find_fastest_way(_all_ways):
+    """
+    Finds the fastest way in the arrays of ways.
+    :param _all_ways: Array of arrays with different paths.
+    :return: arrays with the fewest elements - the fastest way.
+    """
+    return min(_all_ways)
+
+# Initialize arrays with the labyrinth
 start_index = None
 
 with open("schema.txt", 'r') as file:
@@ -45,7 +102,8 @@ with open("schema.txt", 'r') as file:
             column_index += 1
         line_index += 1
 
-findPath(start_index[0], start_index[1], 1)
+# Flood and save results
+flood(start_index[0], start_index[1], 1)
 
 with open("schema_result.txt", 'w') as file_result:
     for row in matrix:
@@ -56,4 +114,9 @@ with open("schema_result.txt", 'w') as file_result:
             file_result.write(_col + " ")
         file_result.write("\n")
 
-pass
+# Find all paths and choose the fastest one
+all_ways = []
+
+find_all_ways(matrix, start_index[0], start_index[1], 0, [])
+
+print("The fastest way from A to B is: " + str(find_fastest_way(all_ways)))
